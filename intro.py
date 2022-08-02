eb3f331472dd55e98b84513e90a52fe2519f5582
rodeok <nationx22@gmail.com>
2022-08-02 13:58:50 +0100

    update 1

--- intro.py
+++ intro.py
@@ -1,1 +1,50 @@
-print("hello + world")
+# x = 2
+# y = 3
+# z = 4
+# print(x)
+# print(y)
+# print(z)
+
+# x, y, z = 2,3,4
+# print(x)
+# print(y)
+# print(z)
+
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+
+# ice = "ice network"
+# print(ice)
+# num = 23 * 45
+# print(num)
+# x = 5
+# y = "John"
+# print(x + y)
+
+# x = 5
+# y = "John"
+# print(x, y)
+# x = "awesome"
+
+# def iceNet():
+#    print("Python is " + x)
+
+# iceNet()
+x = "awesome"
+x = "fantastic"
+
+def myfunc():
+  print(x + x)
+
+myfunc()
+
+print("Python is " + x)
